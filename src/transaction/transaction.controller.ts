e41062fa1ca1d5a2f9039913a68882d0fa76e428
Sergio Marcilla <sergio@devgurus.io>
2023-02-23 11:36:02 +0100

    feat(transactions): adds import transacions from files source

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+import { FileData } from 'easy-personal-finance/lib/types/transactions.type';
+import { TransactionFilePipe } from './files.pipe';
 import { TransactionService } from './transaction.service';
 
 @Controller('transaction')
@@ -8,5 +17,14 @@ export class TransactionController {
   @Get()
   find() {
     return this.service.find();
+  }
+
+  @Post('import')
+  @UseInterceptors(AnyFilesInterceptor())
+  importTransactions(
+    @UploadedFiles(TransactionFilePipe) files: Array<FileData>,
+  ) {
+    console.log(files);
+    this.service.importTransactions(files);
   }
 }
